0d78e5b5b31ddf31949926fc3e3535303928f515
teerasak <teerasak.jiar@kmutt.ac.th>
2022-09-04 19:24:36 +0700

    Success

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,27 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
-}
+  let arr = [n1, n2, n3];
+  if (n1<n2&&n1<n3) {
+    if (n2<n3) {
+      arr = [n1, n2, n3];
+    } else {
+      arr = [n1, n3, n2];
+    }
+  } else if (n2<n1&&n2<n3) {
+    if (n1<n3) {
+      arr = [n2, n1, n3];
+    } else {
+      arr = [n2, n3, n1];
+    }
+  } else {
+    if (n1<n2) {
+      arr = [n3, n1, n2];
+    } else {
+      arr = [n3, n2, n1];
+    }
+  }
+  console.log(`{min: ${arr[0]}, mid: ${arr[1]}, max: ${arr[2]}}`);
+} 
 module.exports = minMedMax
+// (minMedMax(-1, 75, 23));
